a4846d3a0053ff65f516f7a2587f54f5c135445e
Alex Nichol <unixpickle@gmail.com>
2018-12-01 21:13:20 -0500

    print hollow hierarchy

--- descriptors/main.c
+++ descriptors/main.c
@@ -6,6 +6,7 @@
 
 void print_descriptors(libusb_device_handle* handle);
 const char* string_descriptor(libusb_device_handle* handle, uint8_t idx);
+void print_sub_descriptor(desc_header_t* header);
 
 int main() {
   printf("initializing libusb...\n");
@@ -36,8 +37,8 @@ void print_descriptors(libusb_device_handle* handle) {
   int res = libusb_control_transfer(
       handle,
       REQUEST_TYPE_DEV_TO_HOST | REQUEST_TYPE_STANDARD | REQUEST_TYPE_DEVICE,
-      REQUEST_GET_DESCRIPTOR, DESC_TYPE_DEVICE, 0, (unsigned char*)&device_desc,
-      sizeof(device_desc), 0);
+      REQUEST_GET_DESCRIPTOR, (DESC_TYPE_DEVICE << 8), 0,
+      (unsigned char*)&device_desc, sizeof(device_desc), 0);
   if (res < 0) {
     fprintf(stderr, "error getting device description: %s (%d)\n",
             libusb_error_name(res), res);
@@ -50,6 +51,30 @@ void print_descriptors(libusb_device_handle* handle) {
          string_descriptor(handle, device_desc.serial_index));
   printf(" - class: %d\n", (int)device_desc.device_class);
   printf(" - subclass: %d\n", (int)device_desc.device_subclass);
+
+  for (int i = 0; i < (int)device_desc.num_configurations; ++i) {
+    unsigned char config_data[1024];
+    res = libusb_control_transfer(
+        handle,
+        REQUEST_TYPE_DEV_TO_HOST | REQUEST_TYPE_STANDARD | REQUEST_TYPE_DEVICE,
+        REQUEST_GET_DESCRIPTOR, (DESC_TYPE_CONFIG << 8) | (uint16_t)i, 0,
+        config_data, sizeof(config_data), 0);
+    if (res < 0) {
+      fprintf(stderr, "error getting configuration description: %s (%d)\n",
+              libusb_error_name(res), res);
+      continue;
+    }
+    int offset = 0;
+    while (offset + 2 < res) {
+      desc_header_t* header = (desc_header_t*)&config_data[offset];
+      if (header->length + offset > res) {
+        fprintf(stderr, "buffer underflow\n");
+        break;
+      }
+      print_sub_descriptor(header);
+      offset += header->length;
+    }
+  }
 }
 
 const char* string_descriptor(libusb_device_handle* handle, uint8_t idx) {
@@ -58,4 +83,16 @@ const char* string_descriptor(libusb_device_handle* handle, uint8_t idx) {
   libusb_get_string_descriptor_ascii(handle, idx, (unsigned char*)result,
                                      sizeof(result));
   return result;
+}
+
+void print_sub_descriptor(desc_header_t* header) {
+  if (header->desc_type == DESC_TYPE_CONFIG) {
+    printf("   - Configuration\n");
+  } else if (header->desc_type == DESC_TYPE_INTERFACE) {
+    printf("     - Interface\n");
+  } else if (header->desc_type == DESC_TYPE_ENDPOINT) {
+    printf("       - Endpoint\n");
+  } else {
+    printf("       - (Unknown descriptor type: %d)\n", header->desc_type);
+  }
 }
