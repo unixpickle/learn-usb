31fbf9cd69e4bfd1826cdaea7507cf3691c8f97c
Alex Nichol <unixpickle@gmail.com>
2018-12-03 12:04:25 -0500

    drafted mass storage command APIs

--- mass_storage/commands.c
+++ mass_storage/commands.c
@@ -8,11 +8,133 @@
 void cbw_packet_convert_endian(cbw_packet_t* packet) {
   packet->signature = le32(packet->signature);
   packet->tag = le32(packet->tag);
-  packet->xfer_length = le32(packet->xfer_length);
+  packet->transfer_len = le32(packet->transfer_len);
 }
 
 void csw_packet_convert_endian(csw_packet_t* packet) {
   packet->signature = le32(packet->signature);
   packet->tag = le32(packet->tag);
   packet->residue = le32(packet->residue);
+}
+
+int ms_dev_in_endpoint(ms_dev_t* dev) {
+  for (int i = 0; i < (int)dev->iface->bNumEndpoints; ++i) {
+    struct libusb_endpoint_descriptor* desc = &dev->iface->endpoint[i];
+    if (desc->bEndpointAddress & 0x80) {
+      return desc->bEndpointAddress;
+    }
+  }
+  return -1;
+}
+
+int ms_dev_out_endpoint(ms_dev_t* dev) {
+  for (int i = 0; i < (int)dev->iface->bNumEndpoints; ++i) {
+    struct libusb_endpoint_descriptor* desc = &dev->iface->endpoint[i];
+    if (!(desc->bEndpointAddress & 0x80)) {
+      return desc->bEndpointAddress;
+    }
+  }
+  return -1;
+}
+
+int ms_dev_out(ms_dev_t* dev,
+               uint32_t data_len,
+               uint8_t command_len,
+               const char* command,
+               const char* data) {
+  cbw_packet_t cbw;
+  cbw.signature = 0x43425355;
+  cbw.tag = 1337;
+  cbw.transfer_len = data_len;
+  cbw.flags = 0;
+  cbw.lun = dev->lun;
+  cbw.command_len = command_len;
+  assert(command_len <= sizeof(cbw.command));
+  memcpy(cbw.command, command, command_len);
+  cbw_packet_convert_endian(&cbw);
+
+  int out_endpoint = ms_dev_out_endpoint(dev);
+  int in_endpoint = ms_dev_in_endpoint(dev);
+  if (out_endpoint < 0 || in_endpoint < 0) {
+    return -LIBUSB_ERROR_PIPE;
+  }
+  int actual_length = 0;
+  int res = libusb_bulk_transfer(dev->handle, out_endpoint, &cbw, sizeof(cbw),
+                                 &actual_length, 0);
+  if (res) {
+    return -res;
+  }
+
+  int res = libusb_bulk_transfer(dev->handle, out_endpoint, data, (int)data_len,
+                                 &actual_length, 0);
+  if (res) {
+    return -res;
+  }
+
+  csw_packet_t csw;
+  int res = libusb_bulk_transfer(dev->handle, in_endpoint, &csw, sizeof(csw),
+                                 &actual_length, 0);
+  if (res) {
+    return -res;
+  }
+  csw_packet_convert_endian(&csw);
+
+  if (csw.tag != 1337) {
+    return -LIBUSB_ERROR_OTHER;
+  }
+  if (csw.status) {
+    return -LIBUSB_ERROR_IO;
+  }
+  return data_len - csw.residue;
+}
+
+int ms_dev_in(ms_dev_t* dev,
+              uint32_t data_len,
+              uint8_t command_len,
+              const char* command,
+              const char* data) {
+  cbw_packet_t cbw;
+  cbw.signature = 0x43425355;
+  cbw.tag = 1338;
+  cbw.transfer_len = data_len;
+  cbw.flags = 0x80;
+  cbw.lun = dev->lun;
+  cbw.command_len = command_len;
+  assert(command_len <= sizeof(cbw.command));
+  memcpy(cbw.command, command, command_len);
+  cbw_packet_convert_endian(&cbw);
+
+  int out_endpoint = ms_dev_out_endpoint(dev);
+  int in_endpoint = ms_dev_in_endpoint(dev);
+  if (out_endpoint < 0 || in_endpoint < 0) {
+    return -LIBUSB_ERROR_PIPE;
+  }
+  int actual_length = 0;
+  int res = libusb_bulk_transfer(dev->handle, out_endpoint, &cbw, sizeof(cbw),
+                                 &actual_length, 0);
+  if (res) {
+    return -res;
+  }
+
+  int res = libusb_bulk_transfer(dev->handle, in_endpoint, data, (int)data_len,
+                                 &actual_length, 0);
+  if (res) {
+    return -res;
+  }
+
+  csw_packet_t csw;
+  int res = libusb_bulk_transfer(dev->handle, in_endpoint, &csw, sizeof(csw),
+                                 &actual_length, 0);
+  if (res) {
+    return -res;
+  }
+  csw_packet_convert_endian(&csw);
+
+  if (csw.tag != 1338) {
+    return -LIBUSB_ERROR_OTHER;
+  }
+  if (csw.status) {
+    return -LIBUSB_ERROR_IO;
+  }
+  return data_len - csw.residue;
 }
--- mass_storage/commands.h
+++ mass_storage/commands.h
@@ -1,5 +1,5 @@
-#ifndef __MS_COMMANDS_H__
-#define __MS_COMMANDS_H__
+#ifndef __ms_devS_H__
+#define __ms_devS_H__
 
 #include <inttypes.h>
 #include <libusb.h>
@@ -7,11 +7,11 @@
 typedef struct __attribute__((packed)) {
   uint32_t signature;
   uint32_t tag;
-  uint32_t xfer_length;
+  uint32_t transfer_len;
   uint8_t flags;
   uint8_t lun;
   uint8_t command_len;
-  unsigned char command[16];
+  char command[16];
 } cbw_packet_t;
 
 typedef struct __attribute__((packed)) {
@@ -23,5 +23,32 @@ typedef struct __attribute__((packed)) {
 
 void cbw_packet_convert_endian(cbw_packet_t* packet);
 void csw_packet_convert_endian(csw_packet_t* packet);
+
+typedef struct {
+  libusb_device_handle* handle;
+  struct libusb_interface_descriptor* iface;
+  uint32_t lun;
+} ms_dev_t;
+
+int ms_dev_in_endpoint(ms_dev_t* dev);
+int ms_dev_out_endpoint(ms_dev_t* dev);
+
+// Transfer data to the device.
+// Returns a negative value on failure.
+// Otherwise, returns the number of bytes sent.
+int ms_dev_out(ms_dev_t* dev,
+               uint32_t data_len,
+               uint8_t command_len,
+               const char* command,
+               const char* data);
+
+// Transfer data from the device.
+// Returns a negative value on failure.
+// Otherwise, returns the number of bytes read.
+int ms_dev_in(ms_dev_t* dev,
+              uint32_t data_len,
+              uint8_t command_len,
+              const char* command,
+              const char* data);
 
 #endif
